3e1da3390622caa3cbd3f6349ce5ca54693e85f5
Scott Chandler <gsc@woolybeastsoftware.com>
2013-04-12 21:17:17 -0700

    Initial Commit

---
+++ SideSwipeView/WBViewController.h
@@ -0,0 +1,13 @@
+//
+//  WBViewController.h
+//  SideSwipeView
+//
+//  Created by Scott Chandler on 4/12/13.
+//  Copyright (c) 2013 Wooly Beast Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBViewController : UIViewController
+
+@end
